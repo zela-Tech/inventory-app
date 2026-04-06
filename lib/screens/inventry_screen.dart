ce822865d2f8a44061626dc5d8f25f20614d1cc9
Adom Z <zelalemadom@gmail.com>
2026-04-06 14:36:27 -0400

    feat:connect invetory screen to Firestore stream with loading and error states

--- lib/screens/inventry_screen.dart
+++ lib/screens/inventry_screen.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
+import '../models/item_model.dart';
+import '../services/firestore_service.dart';
 
 class InventoryScreen extends StatelessWidget {
   const InventoryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final service = FirestoreService();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Inventory'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: const Center(
-        child: Text('Inventory Screen'),
+      body: StreamBuilder<List<Item>>(
+        stream: service.streamItems(),
+        builder: (context, snapshot) {
+          // loading
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          //in the cas of and rror 
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+          return const SizedBox(); 
+        },
       ),
     );
   }
